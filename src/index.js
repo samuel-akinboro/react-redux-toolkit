cb07f8737d5eb5005c445071411aa0129a68f8d4
samuel akinboro <victorakinboro2003@gmail.com>
2022-09-24 14:12:39 +0000

    basic store setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { Provider } from 'react-redux'
 import App from "./App";
-
+import { store } from './store/store'
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </StrictMode>
 );
